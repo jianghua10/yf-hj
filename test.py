215bf8d6ef32fd0784ba3e5cf57d9aac9481de25
jianghua10 <jh602019488@gmail.com>
2022-01-08 15:33:44 +0100

    八个指标的均值

--- test.py
+++ test.py
@@ -35,6 +35,7 @@ train_data = data[data.index.isin(train_index)]
 test_data = data[data.index.isin(test_index)]
 
 x_t = train_data.iloc[:,0:-8]
+y_train_mean = train_data.iloc[:,-8:].mean(axis=1)
 y_t_1 = train_data.iloc[:,-8]
 y_t_2 = train_data.iloc[:,-7]
 y_t_3 = train_data.iloc[:,-6]
@@ -45,6 +46,7 @@ y_t_7 = train_data.iloc[:,-2]
 y_t_8 = train_data.iloc[:,-1]
 
 x = test_data.iloc[:,0:-8]
+y_test_mean = test_data.iloc[:,-8:].mean(axis=1)
 y_1 = test_data.iloc[:,-8]
 y_2 = test_data.iloc[:,-7]
 y_3 = test_data.iloc[:,-6]
@@ -57,11 +59,13 @@ y_8 = test_data.iloc[:,-1]
 #建立变量储存预测准确率结果
 result = []
 result.append(['焦虑情绪','抑郁情绪','烦躁情绪','失眠情况',
-               '工作压力','家庭压力','经济压力','人际压力'])
+               '工作压力','家庭压力','经济压力','人际压力','八个指标的均值'])
 #开始模型学习和预测
 #XGB模型
 from xgboost import XGBClassifier
-RFC = XGBClassifier() 
+RFC = XGBClassifier()
+RFC.fit(x_t, y_train_mean) #new
+p_y_mean = RFC.predict(x) #new
 RFC.fit(x_t, y_t_1)
 p_y_1 = RFC.predict(x)
 RFC.fit(x_t, y_t_2)
@@ -78,10 +82,12 @@ RFC.fit(x_t, y_t_7)
 p_y_7 = RFC.predict(x)
 RFC.fit(x_t, y_t_8)
 p_y_8 = RFC.predict(x)
-result.append([acs(y_1,p_y_1),acs(y_2,p_y_2),acs(y_3,p_y_3),acs(y_4,p_y_4),acs(y_5,p_y_5),acs(y_6,p_y_6),acs(y_7,p_y_7),acs(y_8,p_y_8)])
+result.append([acs(y_test_mean,p_y_mean),acs(y_1,p_y_1),acs(y_2,p_y_2),acs(y_3,p_y_3),acs(y_4,p_y_4),acs(y_5,p_y_5),acs(y_6,p_y_6),acs(y_7,p_y_7),acs(y_8,p_y_8)])
 #随机森林模型
 from sklearn.ensemble import RandomForestClassifier
 RFC = RandomForestClassifier()
+RFC.fit(x_t, y_train_mean) #new
+p_y_mean = RFC.predict(x) #new
 RFC.fit(x_t, y_t_1)
 p_y_1 = RFC.predict(x)
 RFC.fit(x_t, y_t_2)
@@ -98,10 +104,12 @@ RFC.fit(x_t, y_t_7)
 p_y_7 = RFC.predict(x)
 RFC.fit(x_t, y_t_8)
 p_y_8 = RFC.predict(x)
-result.append([acs(y_1,p_y_1),acs(y_2,p_y_2),acs(y_3,p_y_3),acs(y_4,p_y_4),acs(y_5,p_y_5),acs(y_6,p_y_6),acs(y_7,p_y_7),acs(y_8,p_y_8)])
+result.append([acs(y_test_mean,p_y_mean),acs(y_1,p_y_1),acs(y_2,p_y_2),acs(y_3,p_y_3),acs(y_4,p_y_4),acs(y_5,p_y_5),acs(y_6,p_y_6),acs(y_7,p_y_7),acs(y_8,p_y_8)])
 #极限提升树模型
 from sklearn.ensemble import ExtraTreesClassifier
 RFC = ExtraTreesClassifier()
+RFC.fit(x_t, y_train_mean) #new
+p_y_mean = RFC.predict(x) #new
 RFC.fit(x_t, y_t_1)
 p_y_1 = RFC.predict(x)
 RFC.fit(x_t, y_t_2)
@@ -118,10 +126,12 @@ RFC.fit(x_t, y_t_7)
 p_y_7 = RFC.predict(x)
 RFC.fit(x_t, y_t_8)
 p_y_8 = RFC.predict(x)
-result.append([acs(y_1,p_y_1),acs(y_2,p_y_2),acs(y_3,p_y_3),acs(y_4,p_y_4),acs(y_5,p_y_5),acs(y_6,p_y_6),acs(y_7,p_y_7),acs(y_8,p_y_8)])
+result.append([acs(y_test_mean,p_y_mean),acs(y_1,p_y_1),acs(y_2,p_y_2),acs(y_3,p_y_3),acs(y_4,p_y_4),acs(y_5,p_y_5),acs(y_6,p_y_6),acs(y_7,p_y_7),acs(y_8,p_y_8)])
 #支持向量机模型
 from sklearn.svm import SVC
 RFC = SVC()
+RFC.fit(x_t, y_train_mean) #new
+p_y_mean = RFC.predict(x) #new
 RFC.fit(x_t, y_t_1)
 p_y_1 = RFC.predict(x)
 RFC.fit(x_t, y_t_2)
@@ -138,10 +148,12 @@ RFC.fit(x_t, y_t_7)
 p_y_7 = RFC.predict(x)
 RFC.fit(x_t, y_t_8)
 p_y_8 = RFC.predict(x)
-result.append([acs(y_1,p_y_1),acs(y_2,p_y_2),acs(y_3,p_y_3),acs(y_4,p_y_4),acs(y_5,p_y_5),acs(y_6,p_y_6),acs(y_7,p_y_7),acs(y_8,p_y_8)])
+result.append([acs(y_test_mean,p_y_mean),acs(y_1,p_y_1),acs(y_2,p_y_2),acs(y_3,p_y_3),acs(y_4,p_y_4),acs(y_5,p_y_5),acs(y_6,p_y_6),acs(y_7,p_y_7),acs(y_8,p_y_8)])
 #逻辑斯蒂模型
 from sklearn.linear_model import LogisticRegression
 RFC = LogisticRegression(solver ='liblinear')
+RFC.fit(x_t, y_train_mean) #new
+p_y_mean = RFC.predict(x) #new
 RFC.fit(x_t, y_t_1)
 p_y_1 = RFC.predict(x)
 RFC.fit(x_t, y_t_2)
@@ -158,7 +170,7 @@ RFC.fit(x_t, y_t_7)
 p_y_7 = RFC.predict(x)
 RFC.fit(x_t, y_t_8)
 p_y_8 = RFC.predict(x)
-result.append([acs(y_1,p_y_1),acs(y_2,p_y_2),acs(y_3,p_y_3),acs(y_4,p_y_4),acs(y_5,p_y_5),acs(y_6,p_y_6),acs(y_7,p_y_7),acs(y_8,p_y_8)])
+result.append([acs(y_test_mean,p_y_mean),acs(y_1,p_y_1),acs(y_2,p_y_2),acs(y_3,p_y_3),acs(y_4,p_y_4),acs(y_5,p_y_5),acs(y_6,p_y_6),acs(y_7,p_y_7),acs(y_8,p_y_8)])
 #%%
 result = pd.DataFrame(result).T
 result.columns = ['目标','XGB','RFC','ETC','SVC','LOG']
@@ -174,6 +186,11 @@ plt.rcParams['font.sans-serif']=['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
 
 #%%
+RFC.fit(x_t, y_test_mean)
+explainer = shap.Explainer(RFC,x_t)
+shap_values = explainer(x_t)
+shap.plots.bar(shap_values,max_display=20)
+
 RFC.fit(x_t, y_t_1)
 explainer = shap.Explainer(RFC,x_t)
 shap_values = explainer(x_t) 
